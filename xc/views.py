c6b6e5ce2e10daf5dcd3d202bf5cd4d41d7451d1
Alex Kim <agkim@lbl.gov>
2021-08-21 20:24:10 -0700

    works

--- xc/views.py
+++ xc/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Proposal
 
-# Create your views here.
+def proposal_list(request):
+    proposals = Proposal.objects.all()
+    return render(request, 'xc/proposal_list.html', {'proposals': proposals})
