abe9db71234fd585bec0d460a2d4a4ed4d7f73ba
AlexGKim <agkim@lbl.gov>
2021-08-22 09:53:17 -0700

    Update settings.py

--- desi_xc/settings.py
+++ desi_xc/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'desi_xc.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #        'ENGINE': 'django_postgres_extensions.backends.postgresql',
 #        'NAME': 'db',
 #        'USER': 'postgres',
