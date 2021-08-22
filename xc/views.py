5ccea2446a6501bd4460e717458b36b3a6a1001e
Alex Kim <agkim@lbl.gov>
2021-08-22 10:49:15 -0700

    authentication

--- xc/views.py
+++ xc/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django_tables2 import SingleTableView
 from .models import Proposal
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .tables import ProposalTable
 
 def proposal_list(request):
     proposals = Proposal.objects.all()
     return render(request, 'xc/proposal_list.html', {'proposals': proposals})
 
-class ProposalListView(SingleTableView):
+class ProposalListView(LoginRequiredMixin, SingleTableView):
     model = Proposal
     table_class = ProposalTable
     template_name = 'xc/proposal.html'
