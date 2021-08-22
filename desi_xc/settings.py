9ca38b74b4a498bef107307f1cb7bfba09f7f7e4
Alex Kim <agkim@lbl.gov>
2021-08-22 10:55:36 -0700

    media

--- desi_xc/settings.py
+++ desi_xc/settings.py
@@ -130,5 +130,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # config/settings.py
 LOGIN_REDIRECT_URL = '/'
